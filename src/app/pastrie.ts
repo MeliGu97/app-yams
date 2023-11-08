678297700dc5fc733b099f8d0498c951dad888ac
MeliGu97 <melissandre1997@laposte.net>
2023-11-08 16:45:43 +0100

    avec le OnSelect fonctionnel

---
+++ src/app/pastrie.ts
@@ -0,0 +1,23 @@
+export class Pastrie {
+    id: string;
+    ref: string;
+    name: string;
+    description: string;
+    quantity: number;
+    order: number;
+    url?: string;
+    tags?: string[];
+    like?: string;
+    
+    constructor(id: string, ref: string, name: string, description: string, quantity: number, order: number, url?: string, tags?: string[], like?: string) {
+        this.id = id;
+        this.ref = ref;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.order = order;
+        this.url = url;
+        this.tags = tags;
+        this.like = like;
+    }
+}
